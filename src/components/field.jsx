7d33738f6fcce835bef74663063c9527b78966ae
sovsianykov <swebproger@gmail.com>
2021-04-21 23:44:28 +0300

    Revert "rocket and ball is ready1"
    This reverts commit 5da6cc1c

--- src/components/field.jsx
+++ src/components/field.jsx
@@ -1,20 +1,14 @@
-import React, {useEffect, useRef, useState} from 'react'
+import React, {useEffect, useRef} from 'react'
 import {Button} from "@material-ui/core";
-import {fromEvent, Observable} from "rxjs";
+import { fromEvent, Observable } from "rxjs";
 import { map } from 'rxjs/operators'
-import {observe} from "web-vitals/dist/modules/lib/observe";
 const Field = () => {
-
-    const [rocketCord, setRocketCord ] = useState({})
-    const [ballCord, setBallCord ] = useState({})
 const ball = useRef(null)
 const field = useRef(null)
 const rocket = useRef(null)
-
     useEffect(() =>{
         fromEvent(field.current,'click')
             .subscribe(() =>{handleClick()})
-
     },[])
 
 
@@ -22,19 +16,20 @@ const rocket = useRef(null)
     let posY =0, posX =0, stepX= 0, stepY = 0
 
   const handleClick = () => {
+
       stepX= 20
          stepY = 30
+
       const interval = setInterval(() => { move()
           }, 16)
 
-      setTimeout(() => { clearInterval(interval)}, 10000);
+      setTimeout(() => { clearInterval(interval);}, 10000);
   }
     function move() {
         coordinates = ball.current.getBoundingClientRect()
         // console.log("Top:"+coordinates.y+", Left:"+coordinates.x)
         posY+= stepY;
         posX+= stepX;
-        setBallCord({x: posX, y : posY})
 
         if (posY > 780 || posY < 0  ) { stepY*= -1 }
         if (posX > 980 || posX < 0  ) { stepX*= -1 }
@@ -47,30 +42,21 @@ const rocket = useRef(null)
 
 
  useEffect(() =>{
- const subscription = fromEvent(field.current,'mousemove')
+     fromEvent(field.current,'mousemove')
          .pipe(
              map(e =>({
                  x: e.offsetX,
                  y: e.offsetY
              }))
+
          )
          .subscribe(event=>{
              rocket.current.style.cssText = `top : ${event.y-80}px; left : ${event.x -100}px`
-             setRocketCord({x: event.x - 100,y: event.y - 80})
+             console.log('Y= ' +event.y,'X= ' +event.x )
 
          })
 
-     return ()=> { subscription.unsubscribe()}
  },[])
-
-    const stream$ = new Observable((observer) =>{
-        observer.next(ballCord)
-
-    })
-     stream$.subscribe(
-         value =>console.log(value)
-     )
-
 
 
     return (
